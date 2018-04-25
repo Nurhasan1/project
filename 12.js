15d4a09b19a03100edc4af49ba56e7be7c94c29c
Nurhasan1 <38733398+Nurhasan1@users.noreply.github.com>
2018-04-26 02:05:11 +0700

    Update 12.js

--- 12.js
+++ 12.js
@@ -25,7 +25,7 @@ if(x=="1"){xxx="tinggalin.html"}
 if(x=="2"){xxx="naikin.html"}
 if(x=="3"){xxx="loncatin.html"}
 if(x=="4"){xxx="bantuin.html"}
-if(x=="5"){xxx="gterusin_25.html"}a_to_ve[i].href="https://terusinlagi.blogspot.co.id/2018/04/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="5"){xxx="gterusin_25.html"}a_to_ve[i].href="+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
